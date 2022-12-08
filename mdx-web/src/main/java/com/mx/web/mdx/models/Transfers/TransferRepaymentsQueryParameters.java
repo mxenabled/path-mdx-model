ae08dc9d0ebbba888c66a90d4173ada7ee269e81
Tyler Groesbeck <tyler.groesbeck@mx.com>
2022-12-08 15:07:34 -0700

    feat: set transfer fee controllers to use query parameters

--- mdx-web/src/main/java/com/mx/web/mdx/models/Transfers/TransferRepaymentsQueryParameters.java
+++ mdx-web/src/main/java/com/mx/web/mdx/models/Transfers/TransferRepaymentsQueryParameters.java
@@ -8,6 +8,5 @@ public class TransferRepaymentsQueryParameters {
 
   // NOTE: Non-standard naming to assist with parameter binding
   private String start_on;
-  private String transfer_type;
   private String frequency_id;
 }
