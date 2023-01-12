390598be2d94a715ff736d8cc9472d1c164032ed
Greg Felix <greg.felix@mx.com>
2023-01-12 10:04:14 -0700

    fix: removing MdxApiExceptions

--- realtime/src/test/groovy/com/mx/path/api/lib/realtime/UserCreateRequestTest.groovy
+++ realtime/src/test/groovy/com/mx/path/api/lib/realtime/UserCreateRequestTest.groovy
@@ -1,11 +1,11 @@
 package com.mx.path.api.lib.realtime
 
 import com.google.gson.Gson
+import com.mx.common.accessors.UpstreamSystemUnavailable
 import com.mx.common.http.HttpStatus
 import com.mx.path.api.connect.messaging.remote.models.RemoteUser
 import com.mx.path.api.lib.realtime.models.MdxUserWrapper
 import com.mx.path.gateway.net.Response
-import com.mx.path.gateway.util.MdxApiException
 
 import spock.lang.Specification
 
@@ -87,7 +87,7 @@ class UserCreateRequestTest extends Specification {
     request.completed(response)
 
     then:
-    def e = thrown(MdxApiException)
+    def e = thrown(UpstreamSystemUnavailable)
     e.message == "Error checking/creating Mdx user"
   }
 
