3430e2ef56f0547e56bf2109b5672519b04664b8
Matt Nichols <Matthew.Nichols@mx.com>
2023-02-01 08:59:03 -0700

    feat: use session.current to get user id in UserIdProvider
    refactor: mark UserIdProvider.setUserIdProvider deprecated

--- mdx-models/src/main/java/com/mx/models/UserIdProvider.java
+++ mdx-models/src/main/java/com/mx/models/UserIdProvider.java
@@ -3,33 +3,26 @@ package com.mx.models;
 import java.util.function.Supplier;
 
 import com.mx.common.models.MdxBase;
+import com.mx.path.model.context.Session;
 
 /**
  * User ID Provider
  *
- * This static class contains a user id supplier that can be provided by application.
+ * This static class uses the current session to the current user's id and put it on provided objects.
  * This provider is called in the constructor of Mdx Models that need a user id.
- *
- * Example:
- * <pre>
- * UserIdProvider.setUserIdProvider(() -> {
- *   if (Session.current() != null) {
- *     return Session.current().getUserId();
- *   }
- *   return null;
- * });
- * </pre>
  */
 public class UserIdProvider {
-  private static Supplier<String> userIdProvider = null;
-
   public static void setUserId(MdxBase<?> obj) {
-    if (userIdProvider != null) {
-      obj.setUserId(userIdProvider.get());
+    if (Session.current() != null) {
+      obj.setUserId(Session.current().getUserId());
     }
   }
 
+  /**
+   * @deprecated This no longer any affect. Now uses Session.current()
+   * todo: V2 - remove
+   */
+  @Deprecated
   public static void setUserIdProvider(Supplier<String> newUserIdProvider) {
-    userIdProvider = newUserIdProvider;
   }
 }
