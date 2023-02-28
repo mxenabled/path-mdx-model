c1fa9754f3b8d060c04894fa78bdf8877db8fa6c
Matt Nichols <Matthew.Nichols@mx.com>
2023-02-28 16:57:40 -0700

    feat!: move RequireAuthenticationFilter

---
+++ mdx-web/src/main/java/com/mx/path/model/mdx/web/filter/RequireAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.mx.path.model.mdx.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mx.common.accessors.UnauthorizedException;
+import com.mx.path.model.context.Session;
+import com.mx.path.model.context.Session.SessionState;
+import com.mx.path.model.mdx.web.FilterOrderSequence;
+import com.mx.path.model.mdx.web.PathTools;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Require Authentication Filter
+ *
+ * This filter checks for a current session on all routes under
+ * :clientId/users/* For this to function properly, the SessionFilter must have
+ * a higher precedence that this.
+ */
+@Component
+@Order(FilterOrderSequence.AUTH_FILTER)
+public class RequireAuthenticationFilter extends OncePerRequestFilter {
+
+  @Override
+  protected final void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    String path = request.getServletPath();
+
+    // Check session required
+    if (PathTools.isSessionRequiredPath(path)
+        && Session.current() == null) {
+
+      throw new UnauthorizedException("Session not found", "Session not found");
+    }
+
+    if (PathTools.isAuthenticationRequiredPath(path)
+        && (Session.current() == null
+            || Session.current().getSessionState() != SessionState.AUTHENTICATED)) {
+
+      throw new UnauthorizedException("Not Authenticated", "Not Authenticated");
+    }
+
+    filterChain.doFilter(request, response);
+  }
+}
---
+++ mdx-web/src/test/groovy/com/mx/path/model/mdx/web/filter/RequireAuthenticationFilterTest.groovy
@@ -0,0 +1,124 @@
+package com.mx.path.model.mdx.web.filter
+
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+import com.mx.common.accessors.UnauthorizedException
+import com.mx.path.model.context.Session
+import com.mx.path.model.context.store.SessionRepository
+
+import spock.lang.Specification
+
+class RequireAuthenticationFilterTest extends Specification {
+  FilterChain filterChain
+  HttpServletRequest request
+  SessionRepository repository
+  HttpServletResponse response
+  RequireAuthenticationFilter subject
+
+  def setup() {
+    filterChain = Mock()
+    request = Mock()
+    response = Mock()
+    repository = Mock()
+    Session.setRepositorySupplier({ -> repository })
+    subject = new RequireAuthenticationFilter()
+  }
+
+  def cleanup() {
+    Session.clearSession()
+    Session.setRepositorySupplier(null)
+  }
+
+  def "authenticateWithNoSession"() {
+    expect:
+    Session.clearSession()
+    setPath("/client/authentications")
+    subject.doFilter(request, response, filterChain)
+  }
+
+  def "authenticateWithSession"() {
+    expect:
+    Session.createSession()
+    setPath("/client/authentications")
+    subject.doFilter(request, response, filterChain)
+  }
+
+  def "respondToMfaWithSessionInPinToPasswordState"() {
+    expect:
+    Session.createSession()
+    Session.current().setSessionState(Session.SessionState.PINTOPASSWORD)
+    setPath("/client/authentications/session-1234")
+    subject.doFilter(request, response, filterChain)
+  }
+
+  def "respondToMfaWithSessionInChallengedState"() {
+    expect:
+    Session.createSession()
+    Session.current().setSessionState(Session.SessionState.CHALLENGED)
+    setPath("/client/authentications/session-1234")
+    subject.doFilter(request, response, filterChain)
+  }
+
+  def "respondToMfaWithSessionInUnauthenticatedState"() {
+    expect:
+    Session.createSession()
+    Session.current().setSessionState(Session.SessionState.UNAUTHENTICATED)
+    setPath("/client/authentications/session-1234")
+    subject.doFilter(request, response, filterChain)
+  }
+
+  def "respondToMfaWithNoSession"() {
+    given:
+    Session.clearSession()
+    setPath("/client/authentications/session-1234")
+
+    when:
+    subject.doFilter(request, response, filterChain)
+
+    then:
+    thrown(UnauthorizedException)
+  }
+
+  def "userRouteInChallengedState"() {
+    given:
+    Session.createSession()
+    Session.current().setUserId("user123")
+    Session.current().setSessionState(Session.SessionState.CHALLENGED)
+    setPath("/client/users/user123/accounts")
+
+    when:
+    subject.doFilter(request, response, filterChain)
+
+    then:
+    thrown(UnauthorizedException)
+  }
+
+  def "userRouteInPinToPasswordState"() {
+    given:
+    Session.createSession()
+    Session.current().setUserId("user123")
+    Session.current().setSessionState(Session.SessionState.PINTOPASSWORD)
+    setPath("/client/users/user123/accounts")
+
+    when:
+    subject.doFilter(request, response, filterChain)
+
+    then:
+    thrown(UnauthorizedException)
+  }
+
+  def "userRouteInAuthenticatedState"() {
+    expect:
+    Session.createSession()
+    Session.current().setUserId("user123")
+    Session.current().setSessionState(Session.SessionState.AUTHENTICATED)
+    setPath("/client/users/user123/accounts")
+    subject.doFilter(request, response, filterChain)
+  }
+
+  private void setPath(String path) {
+    request.getServletPath() >> path
+  }
+}
