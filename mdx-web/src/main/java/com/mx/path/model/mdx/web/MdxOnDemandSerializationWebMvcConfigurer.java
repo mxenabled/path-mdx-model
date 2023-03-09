b6a9d26405033c2cbe1bd7065fe70e739ee6310a
Matt Nichols <Matthew.Nichols@mx.com>
2023-03-09 07:31:15 -0700

    refactor: move serialization components from mdx-web

---
+++ mdx-web/src/main/java/com/mx/path/model/mdx/web/MdxOnDemandSerializationWebMvcConfigurer.java
@@ -0,0 +1,52 @@
+package com.mx.path.model.mdx.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.mx.models.Resources;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MdxOnDemandSerializationWebMvcConfigurer implements WebMvcConfigurer {
+  /**
+   * List of all configured converts. All others will be removed before inserting custom MappingJackson2XmlHttpMessageConverter
+   */
+  static final List<Class<?>> CONVERT_CLASSES;
+  static {
+    CONVERT_CLASSES = new ArrayList<>();
+    CONVERT_CLASSES.add(GsonHttpMessageConverter.class);
+    CONVERT_CLASSES.add(StringHttpMessageConverter.class);
+  }
+
+  @Override
+  public final void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+    // Get rid of unused converters
+    // These get in the way of the other
+    List<HttpMessageConverter<?>> toRemove = converters.stream().filter(t -> !CONVERT_CLASSES.contains(t.getClass())).collect(Collectors.toCollection(ArrayList::new));
+    converters.removeAll(toRemove);
+
+    // XML
+    converters.add(new MappingJackson2XmlHttpMessageConverter(Jackson2ObjectMapperBuilder
+        .xml()
+        .modules(payloadModule())
+        .build()));
+  }
+
+  public final SimpleModule payloadModule() {
+    SimpleModule module = new SimpleModule();
+
+    Resources.registerOnDemandResources(module);
+
+    return module;
+  }
+}
---
+++ mdx-web/src/test/groovy/com/mx/path/model/mdx/web/MdxSerializerFactoryBeanTest.groovy
@@ -0,0 +1,21 @@
+package com.mx.path.model.mdx.web;
+
+import spock.lang.Specification
+
+class MdxSerializerFactoryBeanTest extends Specification {
+  MdxSerializerFactoryBean subject
+
+  def setup() {
+    subject = new MdxSerializerFactoryBean()
+  }
+
+  def "testGetsGsonSerializer"() {
+    expect:
+    subject.getObject() != null
+  }
+
+  def "testIsNotSingleton"() {
+    expect:
+    !subject.isSingleton()
+  }
+}
