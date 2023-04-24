995d5f9564521004f25fa71f1f7fbf3a65ead160
Tess Stoddard <tess.stoddard@mx.com>
2023-04-24 14:22:41 -0600

    fix: Add account model relation to cross account transfers

--- mdx-models/src/main/java/com/mx/path/model/mdx/model/cross_account_transfer/CrossAccountTransfer.java
+++ mdx-models/src/main/java/com/mx/path/model/mdx/model/cross_account_transfer/CrossAccountTransfer.java
@@ -2,9 +2,17 @@ package com.mx.path.model.mdx.model.cross_account_transfer;
 
 import java.time.LocalDate;
 
-import com.mx.common.models.MdxBase;
-import com.mx.path.model.mdx.model.UserIdProvider;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+import com.mx.common.models.MdxBase;
+import com.mx.path.model.mdx.model.MdxRelationId;
+import com.mx.path.model.mdx.model.UserIdProvider;
+import com.mx.path.model.mdx.model.account.Account;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 public final class CrossAccountTransfer extends MdxBase<CrossAccountTransfer> {
   @Deprecated
   private String accountHolder;
@@ -15,6 +23,7 @@ public final class CrossAccountTransfer extends MdxBase<CrossAccountTransfer> {
   private Double amount;
   private String confirmationId;
   private String destinationId;
+  @Getter(onMethod_ = { @MdxRelationId(referredClass = Account.class) })
   private String fromAccountId;
   private String id;
   private String memo;
@@ -27,127 +36,5 @@ public final class CrossAccountTransfer extends MdxBase<CrossAccountTransfer> {
 
   public CrossAccountTransfer() {
     UserIdProvider.setUserId(this);
-  }
-
-  // getter & setters
-
-  public String getAccountHolder() {
-    return accountHolder;
-  }
-
-  public void setAccountHolder(String accountHolder) {
-    this.accountHolder = accountHolder;
-  }
-
-  public String getAccountType() {
-    return accountType;
-  }
-
-  public void setAccountType(String accountType) {
-    this.accountType = accountType;
-  }
-
-  public String getAccountTypeId() {
-    return accountTypeId;
-  }
-
-  public void setAccountTypeId(String accountTypeId) {
-    this.accountTypeId = accountTypeId;
-  }
-
-  public Integer getAccountTypeNumber() {
-    return accountTypeNumber;
-  }
-
-  public void setAccountTypeNumber(Integer accountTypeNumber) {
-    this.accountTypeNumber = accountTypeNumber;
-  }
-
-  public Double getAmount() {
-    return amount;
-  }
-
-  public void setAmount(Double amount) {
-    this.amount = amount;
-  }
-
-  public String getConfirmationId() {
-    return confirmationId;
-  }
-
-  public void setConfirmationId(String confirmationId) {
-    this.confirmationId = confirmationId;
-  }
-
-  public String getDestinationId() {
-    return destinationId;
-  }
-
-  public void setDestinationId(String destinationId) {
-    this.destinationId = destinationId;
-  }
-
-  public String getFromAccountId() {
-    return fromAccountId;
-  }
-
-  public void setFromAccountId(String fromAccountId) {
-    this.fromAccountId = fromAccountId;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getMemo() {
-    return memo;
-  }
-
-  public void setMemo(String memo) {
-    this.memo = memo;
-  }
-
-  public LocalDate getPostOn() {
-    return postOn;
-  }
-
-  public void setPostOn(LocalDate postOn) {
-    this.postOn = postOn;
-  }
-
-  public LocalDate getPostedOn() {
-    return postedOn;
-  }
-
-  public void setPostedOn(LocalDate postedOn) {
-    this.postedOn = postedOn;
-  }
-
-  public String getRecurringCrossAccountTransferId() {
-    return recurringCrossAccountTransferId;
-  }
-
-  public void setRecurringCrossAccountTransferId(String recurringCrossAccountTransferId) {
-    this.recurringCrossAccountTransferId = recurringCrossAccountTransferId;
-  }
-
-  public String getStatus() {
-    return status;
-  }
-
-  public void setStatus(String status) {
-    this.status = status;
-  }
-
-  public String getToAccountNumber() {
-    return toAccountNumber;
-  }
-
-  public void setToAccountNumber(String toAccountNumber) {
-    this.toAccountNumber = toAccountNumber;
   }
 }
