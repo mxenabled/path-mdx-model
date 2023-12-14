a68f83cc7c6b158416a01dade30ea3f84a9a2839
Bruno Martins <bruno.martins@contractors.mx.com>
2023-12-14 09:31:11 -0300

    feat: update password returns password

--- mdx-web/src/main/java/com/mx/path/model/mdx/web/controller/ProfilesController.java
+++ mdx-web/src/main/java/com/mx/path/model/mdx/web/controller/ProfilesController.java
@@ -85,7 +85,7 @@ public class ProfilesController extends BaseController {
     AccessorResponse<ChallengeQuestions> response = gateway().profiles().challengeQuestions().update(challengeQuestions);
     ChallengeQuestions result = response.getResult();
     HttpStatus status = HttpStatus.NO_CONTENT;
-    if (result.getChallenges() != null && result.getChallenges().size() > 0) {
+    if (result != null && result.getChallenges() != null && result.getChallenges().size() > 0) {
       status = HttpStatus.ACCEPTED;
     }
     return new ResponseEntity<>(response.getResult().wrapped(), createMultiMapForResponse(response.getHeaders()), status);
@@ -165,21 +165,25 @@ public class ProfilesController extends BaseController {
   }
 
   @RequestMapping(value = "/users/{userId}/profile/update_password", method = RequestMethod.PUT)
-  public final ResponseEntity<MdxList<Challenge>> updatePassword(@RequestBody Password password) {
-    AccessorResponse<MdxList<Challenge>> response = gateway().profiles().updatePassword(password);
-    if (response.getResult() != null && response.getResult().size() != 0) {
-      return new ResponseEntity<>(response.getResult().wrapped(), createMultiMapForResponse(response.getHeaders()), HttpStatus.ACCEPTED);
+  public final ResponseEntity<Password> updatePassword(@RequestBody Password password) {
+    AccessorResponse<Password> response = gateway().profiles().updatePassword(password);
+    Password result = response.getResult();
+    HttpStatus status = HttpStatus.NO_CONTENT;
+    if (result != null && result.getChallenges() != null && result.getChallenges().size() > 0) {
+      status = HttpStatus.ACCEPTED;
     }
-    return new ResponseEntity<>(createMultiMapForResponse(response.getHeaders()), HttpStatus.NO_CONTENT);
+    return new ResponseEntity<>(response.getResult().wrapped(), createMultiMapForResponse(response.getHeaders()), status);
   }
 
   @RequestMapping(value = "/users/{userId}/profile/update_password/challenges/{challengeId}", method = RequestMethod.PUT)
-  public final ResponseEntity<MdxList<Challenge>> updatePasswordResume(@PathVariable("challengeId") String challengeId, @RequestBody Challenge challenge) {
-    AccessorResponse<MdxList<Challenge>> response = gateway().profiles().updatePasswordResume(challengeId, challenge);
-    if (response.getResult() != null && response.getResult().size() != 0) {
-      return new ResponseEntity<>(response.getResult().wrapped(), createMultiMapForResponse(response.getHeaders()), HttpStatus.ACCEPTED);
+  public final ResponseEntity<Password> updatePasswordResume(@PathVariable("challengeId") String challengeId, @RequestBody Challenge challenge) {
+    AccessorResponse<Password> response = gateway().profiles().updatePasswordResume(challengeId, challenge);
+    Password result = response.getResult();
+    HttpStatus status = HttpStatus.NO_CONTENT;
+    if (result != null && result.getChallenges() != null && result.getChallenges().size() > 0) {
+      status = HttpStatus.ACCEPTED;
     }
-    return new ResponseEntity<>(createMultiMapForResponse(response.getHeaders()), HttpStatus.NO_CONTENT);
+    return new ResponseEntity<>(response.getResult().wrapped(), createMultiMapForResponse(response.getHeaders()), status);
   }
 
   @RequestMapping(value = "/users/{userId}/profile/update_username", method = RequestMethod.PUT)
@@ -187,7 +191,7 @@ public class ProfilesController extends BaseController {
     AccessorResponse<UserName> response = gateway().profiles().updateUserName(updateUserInfo);
     UserName result = response.getResult();
     HttpStatus status = HttpStatus.NO_CONTENT;
-    if (result.getChallenges() != null && result.getChallenges().size() > 0) {
+    if (result != null && result.getChallenges() != null && result.getChallenges().size() > 0) {
       status = HttpStatus.ACCEPTED;
     }
     return new ResponseEntity<>(response.getResult().wrapped(), createMultiMapForResponse(response.getHeaders()), status);
