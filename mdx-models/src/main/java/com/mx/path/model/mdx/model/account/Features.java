95cfc4546cecf49a6369cde7e88053afa4de6129
Neehaarika <neehaarika.dasari@mx.com>
2023-12-18 10:46:47 -0700

    feat: updated fields

--- mdx-models/src/main/java/com/mx/path/model/mdx/model/account/Features.java
+++ mdx-models/src/main/java/com/mx/path/model/mdx/model/account/Features.java
@@ -1,17 +1,21 @@
 package com.mx.path.model.mdx.model.account;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import com.mx.path.model.mdx.model.MdxBase;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class Features extends MdxBase<Features> {
-    private Boolean hasCardManager;
-    private Boolean hasCheckReorder;
-    private Boolean hasEditAddress;
-    private Boolean hasEditNickname;
-    private Boolean hasFeature;
-    private Boolean hasOverdraft;
-    private Boolean hasOverdraftEnrollment;
-    private Boolean hasStatements;
-    private Boolean hasStatementsEnrollment;
-    private Boolean hasStopPayment;
-    private Boolean hasUpdateAccountAddress;
+  private Boolean hasCardManager;
+  private Boolean hasCheckReorder;
+  private Boolean hasEditAddress;
+  private Boolean hasEditNickname;
+  private Boolean hasFeature;
+  private Boolean hasOverdraft;
+  private Boolean hasOverdraftEnrollment;
+  private Boolean hasStatements;
+  private Boolean hasStatementsEnrollment;
+  private Boolean hasStopPayment;
 }
