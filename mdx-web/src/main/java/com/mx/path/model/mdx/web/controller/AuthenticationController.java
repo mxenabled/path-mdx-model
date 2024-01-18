93e21b96ecb5983ebd8264490dd6b62e11390817
Krishna Potluri <krishna.potluri@mx.com>
2024-01-18 11:32:21 -0700

    fix: fixing the status code for reset password challenges list

--- mdx-web/src/main/java/com/mx/path/model/mdx/web/controller/AuthenticationController.java
+++ mdx-web/src/main/java/com/mx/path/model/mdx/web/controller/AuthenticationController.java
@@ -190,7 +190,8 @@ public class AuthenticationController extends BaseController {
     ResetPassword result = response.getResult();
     // Return 202 returning challenge questions
     HttpStatus status = HttpStatus.NO_CONTENT;
-    if (result.getChallenge() != null) {
+    if (result.getChallenge() != null || response.getResult().getChallenges() != null
+        && response.getResult().getChallenges().size() > 0) {
       status = HttpStatus.ACCEPTED;
     }
     return new ResponseEntity<>(result.wrapped(), createMultiMapForResponse(response.getHeaders()), status);
