9c94cd1168e3a39d92f9ac7860f5bdba70903e99
Neehaarika <neehaarika.dasari@mx.com>
2024-02-26 15:10:44 -0800

    feat: adding new field to the model

--- mdx-models/src/main/java/com/mx/path/model/mdx/model/account/Features.java
+++ mdx-models/src/main/java/com/mx/path/model/mdx/model/account/Features.java
@@ -13,6 +13,7 @@ public class Features extends MdxBase<Features> {
   private Boolean hasEditAddress;
   private Boolean hasEditNickname;
   private Boolean hasFeature;
+  private Boolean hasManageStatementsDeliveryPreference;
   private Boolean hasOverdraft;
   private Boolean hasOverdraftEnrollment;
   private Boolean hasStatements;
