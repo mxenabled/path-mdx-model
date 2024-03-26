7dc2a521d08a8d92b65794b2f75c373b07b2be24
Sunshine <bhatia.sandhya01@gmail.com>
2024-03-26 13:30:14 -0600

    adding new model for info_data

---
+++ mdx-models/src/main/java/com/mx/path/model/mdx/model/challenges/Format.java
@@ -0,0 +1,34 @@
+package com.mx.path.model.mdx.model.challenges;
+
+import com.google.gson.annotations.SerializedName;
+import com.mx.path.model.mdx.model.MdxBase;
+
+public final class Format extends MdxBase<Format> {
+  private String font;
+  private String color;
+  @SerializedName("all_upper")
+  private boolean allUpper;
+  public String getFont() {
+    return font;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public boolean isAllUpper() {
+    return allUpper;
+  }
+
+  public void setFont(String font) {
+    this.font = font;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public void setAllUpper(boolean allUpper) {
+    this.allUpper = allUpper;
+  }
+}
