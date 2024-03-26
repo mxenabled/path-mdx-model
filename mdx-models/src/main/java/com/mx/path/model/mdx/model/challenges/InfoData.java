11a8526d9ec2ada0f13edd804cff15e18a12bfc5
Sunshine <bhatia.sandhya01@gmail.com>
2024-03-26 13:30:14 -0600

    adding new model for info_data

---
+++ mdx-models/src/main/java/com/mx/path/model/mdx/model/challenges/InfoData.java
@@ -0,0 +1,15 @@
+package com.mx.path.model.mdx.model.challenges;
+
+import com.mx.path.model.mdx.model.MdxBase;
+
+public final class InfoData extends MdxBase<InfoData> {
+  private Format format;
+
+  public Format getFormat() {
+    return format;
+  }
+
+  public void setFormat(Format format) {
+    this.format = format;
+  }
+}
