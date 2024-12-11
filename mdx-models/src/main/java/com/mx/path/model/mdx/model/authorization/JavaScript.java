084b1f76fbf15379470d29a52dc0a97cf2d339b9
Steven Leighton <steven.leighton@mx.com>
2024-12-11 15:10:48 -0700

    fix: replace authorization getters and setters with lombok annotations

--- mdx-models/src/main/java/com/mx/path/model/mdx/model/authorization/JavaScript.java
+++ mdx-models/src/main/java/com/mx/path/model/mdx/model/authorization/JavaScript.java
@@ -1,27 +1,15 @@
 package com.mx.path.model.mdx.model.authorization;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import com.mx.path.model.mdx.model.MdxBase;
 import com.mx.path.model.mdx.model.MdxNested;
 
 @MdxNested
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class JavaScript extends MdxBase<JavaScript> {
-
   private NameValuePair[] arguments;
   private String functionToInvoke;
-
-  public final NameValuePair[] getArguments() {
-    return arguments;
-  }
-
-  public final void setArguments(NameValuePair[] newArguments) {
-    this.arguments = newArguments;
-  }
-
-  public final String getFunctionToInvoke() {
-    return functionToInvoke;
-  }
-
-  public final void setFunctionToInvoke(String newFunctionToInvoke) {
-    this.functionToInvoke = newFunctionToInvoke;
-  }
 }
