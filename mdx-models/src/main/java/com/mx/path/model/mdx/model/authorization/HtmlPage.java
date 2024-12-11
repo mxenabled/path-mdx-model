34c93622066c9b1445f055ff1074f1261a0d9a6a
Steven Leighton <steven.leighton@mx.com>
2024-12-11 15:10:48 -0700

    fix: replace authorization getters and setters with lombok annotations

--- mdx-models/src/main/java/com/mx/path/model/mdx/model/authorization/HtmlPage.java
+++ mdx-models/src/main/java/com/mx/path/model/mdx/model/authorization/HtmlPage.java
@@ -1,14 +1,14 @@
 package com.mx.path.model.mdx.model.authorization;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import com.mx.path.model.mdx.model.MdxBase;
 import com.mx.path.model.mdx.model.MdxNested;
 
 @MdxNested
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class HtmlPage extends MdxBase<HtmlPage> {
-
   private String content;
-
 }
