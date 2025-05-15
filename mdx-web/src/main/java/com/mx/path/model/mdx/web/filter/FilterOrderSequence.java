b76576695c54284a0a12b86bbcd30a12d20fcb10
tessstoddard <108095550+tessstoddard@users.noreply.github.com>
2025-05-15 15:15:04 -0600

    Revert "feat: add incoming request logging filter"
    This reverts commit 86361a93a3914e46fe18055e8f850efeee971c6b.

--- mdx-web/src/main/java/com/mx/path/model/mdx/web/filter/FilterOrderSequence.java
+++ mdx-web/src/main/java/com/mx/path/model/mdx/web/filter/FilterOrderSequence.java
@@ -2,10 +2,9 @@ package com.mx.path.model.mdx.web.filter;
 
 public class FilterOrderSequence {
   public static final int REQUEST_CONTEXT_FILTER = 1;
-  public static final int REQUEST_LOGGING_FILTER = 2;
-  public static final int ERROR_FILTER = 3;
-  public static final int HMAC_FILTER = 4;
-  public static final int SESSION_FILTER = 5;
-  public static final int AUTH_FILTER = 6;
-  public static final int REPO_FILTER = 7;
+  public static final int ERROR_FILTER = 2;
+  public static final int HMAC_FILTER = 3;
+  public static final int SESSION_FILTER = 4;
+  public static final int AUTH_FILTER = 5;
+  public static final int REPO_FILTER = 6;
 }
