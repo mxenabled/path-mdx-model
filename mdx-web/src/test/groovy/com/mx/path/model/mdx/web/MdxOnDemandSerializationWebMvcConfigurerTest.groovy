f3531839bc204d53a472dc8cf9bbf5065c5bc3d8
Steven Leighton <steven.leighton@mx.com>
2026-05-18 11:50:36 -0600

    fix(mdx-web): restore byte array message converter

---
+++ mdx-web/src/test/groovy/com/mx/path/model/mdx/web/MdxOnDemandSerializationWebMvcConfigurerTest.groovy
@@ -0,0 +1,60 @@
+package com.mx.path.model.mdx.web
+
+import com.fasterxml.jackson.databind.module.SimpleModule
+
+import org.springframework.http.converter.ByteArrayHttpMessageConverter
+import org.springframework.http.converter.FormHttpMessageConverter
+import org.springframework.http.converter.ResourceHttpMessageConverter
+import org.springframework.http.converter.StringHttpMessageConverter
+import org.springframework.http.converter.json.GsonHttpMessageConverter
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter
+
+import spock.lang.Specification
+
+class MdxOnDemandSerializationWebMvcConfigurerTest extends Specification {
+
+  MdxOnDemandSerializationWebMvcConfigurer subject
+
+  def setup() {
+    subject = new MdxOnDemandSerializationWebMvcConfigurer()
+  }
+
+  def "filters non-whitelisted message converters and appends the custom XML converter"() {
+    given:
+    def gsonConverter = new GsonHttpMessageConverter()
+    def stringConverter = new StringHttpMessageConverter()
+    def byteArrayConverter = new ByteArrayHttpMessageConverter()
+    def unwantedResourceConverter = new ResourceHttpMessageConverter()
+    def unwantedFormConverter = new FormHttpMessageConverter()
+
+    def converters = [
+      unwantedResourceConverter,
+      gsonConverter,
+      unwantedFormConverter,
+      stringConverter,
+      byteArrayConverter
+    ]
+
+    when:
+    subject.configureMessageConverters(converters)
+
+    then:
+    verifyAll(converters) {
+      it.size() == 4
+      it.contains(gsonConverter)
+      it.contains(stringConverter)
+      it.contains(byteArrayConverter)
+      !it.contains(unwantedResourceConverter)
+      !it.contains(unwantedFormConverter)
+      it.last() instanceof MappingJackson2XmlHttpMessageConverter
+    }
+  }
+
+  def "payloadModule returns a correctly instantiated SimpleModule"() {
+    when:
+    def module = subject.payloadModule()
+
+    then:
+    module instanceof SimpleModule
+  }
+}
