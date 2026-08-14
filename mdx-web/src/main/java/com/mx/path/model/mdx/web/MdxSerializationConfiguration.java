9df8759ad074417f0f8c246fc43af7f2d02abd7d
Steven Leighton <steven.leighton@mx.com>
2026-08-13 22:10:48 -0600

    fix(mdx-web): restore default message converters

--- mdx-web/src/main/java/com/mx/path/model/mdx/web/MdxSerializationConfiguration.java
+++ mdx-web/src/main/java/com/mx/path/model/mdx/web/MdxSerializationConfiguration.java
@@ -1,32 +1,22 @@
 package com.mx.path.model.mdx.web;
 
-import java.util.List;
-
 import com.google.gson.Gson;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@SuppressWarnings("checkstyle:designforextension")
-public class MdxSerializationConfiguration implements WebMvcConfigurer {
-  public MdxSerializerFactoryBean gsonBeanFactory() {
-    MdxSerializerFactoryBean factory = new MdxSerializerFactoryBean();
-    return factory;
-  }
+public class MdxSerializationConfiguration {
 
+  /**
+   * Provides the customized Gson instance for the application.
+   * Spring Boot automatically detects this Bean and uses it to configure
+   * the default GsonHttpMessageConverter for all JSON serialization.
+   *
+   * @return a fully configured Gson instance
+   */
   @Bean
   public Gson gson() {
-    return gsonBeanFactory().getObject();
-  }
-
-  @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    GsonHttpMessageConverter gsonConverter = new GsonHttpMessageConverter();
-    gsonConverter.setGson(gson());
-    converters.add(gsonConverter);
+    return new MdxSerializerFactoryBean().getObject();
   }
 }
