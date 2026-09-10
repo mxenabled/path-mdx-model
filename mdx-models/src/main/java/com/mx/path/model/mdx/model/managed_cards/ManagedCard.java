290396f1b71d8e912ef6bd0f0495aafb7f868c9f
Tess Stoddard <tess.stoddard@mx.com>
2026-09-10 16:03:50 -0600

    feat: add name field to ManagedCard model

--- mdx-models/src/main/java/com/mx/path/model/mdx/model/managed_cards/ManagedCard.java
+++ mdx-models/src/main/java/com/mx/path/model/mdx/model/managed_cards/ManagedCard.java
@@ -1,5 +1,6 @@
 package com.mx.path.model.mdx.model.managed_cards;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import com.google.gson.annotations.SerializedName;
@@ -7,7 +8,8 @@ import com.mx.path.core.common.model.Internal;
 import com.mx.path.model.mdx.model.MdxBase;
 import com.mx.path.model.mdx.model.challenges.Challenge;
 
-@EqualsAndHashCode
+@Data
+@EqualsAndHashCode(callSuper = true)
 public final class ManagedCard extends MdxBase<ManagedCard> {
 
   private String accountId;
@@ -18,6 +20,7 @@ public final class ManagedCard extends MdxBase<ManagedCard> {
   private String imageUrl;
   private String issuanceType;
   private String maskedNumberOnCard;
+  private String name;
   private String nameOnCard;
   private String pin;
   private String newPin;
@@ -40,158 +43,4 @@ public final class ManagedCard extends MdxBase<ManagedCard> {
   @Internal
   @SerializedName("ondot_ref_id")
   private String ondotRefId;
-
-  public String getAccountId() {
-    return accountId;
-  }
-
-  public void setAccountId(String newAccountId) {
-    this.accountId = newAccountId;
-  }
-
-  public String getAccountType() {
-    return accountType;
-  }
-
-  public void setAccountType(String accountType) {
-    this.accountType = accountType;
-  }
-
-  public Boolean getAllowPushNotification() {
-    return allowPushNotification;
-  }
-
-  public void setAllowPushNotification(Boolean allowPushNotification) {
-    this.allowPushNotification = allowPushNotification;
-  }
-
-  public String getExpirationOnCard() {
-    return expirationOnCard;
-  }
-
-  public void setExpirationOnCard(String newExpirationOnCard) {
-    this.expirationOnCard = newExpirationOnCard;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String newId) {
-    this.id = newId;
-  }
-
-  public String getImageUrl() {
-    return imageUrl;
-  }
-
-  public void setImageUrl(String newImageUrl) {
-    this.imageUrl = newImageUrl;
-  }
-
-  public String getIssuanceType() {
-    return issuanceType;
-  }
-
-  public void setIssuanceType(String newIssuanceType) {
-    this.issuanceType = newIssuanceType;
-  }
-
-  public String getMaskedNumberOnCard() {
-    return maskedNumberOnCard;
-  }
-
-  public void setMaskedNumberOnCard(String newMaskedNumberOnCard) {
-    this.maskedNumberOnCard = newMaskedNumberOnCard;
-  }
-
-  public String getNameOnCard() {
-    return nameOnCard;
-  }
-
-  public void setNameOnCard(String newNameOnCard) {
-    this.nameOnCard = newNameOnCard;
-  }
-
-  public String getPin() {
-    return pin;
-  }
-
-  public void setPin(String pin) {
-    this.pin = pin;
-  }
-
-  public String getNewPin() {
-    return newPin;
-  }
-
-  public void setNewPin(String newPin) {
-    this.newPin = newPin;
-  }
-
-  public String getStatus() {
-    return status;
-  }
-
-  public void setStatus(String newStatus) {
-    this.status = newStatus;
-  }
-
-  public String getStatusMessage() {
-    return statusMessage;
-  }
-
-  public void setStatusMessage(String statusMessage) {
-    this.statusMessage = statusMessage;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String newType) {
-    this.type = newType;
-  }
-
-  public String getUnmaskedNumberOnCard() {
-    return unmaskedNumberOnCard;
-  }
-
-  public void setUnmaskedNumberOnCard(String unmaskedNumberOnCard) {
-    this.unmaskedNumberOnCard = unmaskedNumberOnCard;
-  }
-
-  public String getUnmaskedCvv() {
-    return unmaskedCvv;
-  }
-
-  public void setUnmaskedCvv(String unmaskedCvv) {
-    this.unmaskedCvv = unmaskedCvv;
-  }
-
-  public Challenge[] getChallenges() {
-    return challenges;
-  }
-
-  public void setChallenges(Challenge[] challenges) {
-    this.challenges = challenges;
-  }
-
-  // Internal Fields
-
-  public String getCardNumber() {
-    return cardNumber;
-  }
-
-  public void setCardNumber(String cardNumber) {
-    this.cardNumber = cardNumber;
-  }
-
-  public String getOndotRefId() {
-    return ondotRefId;
-  }
-
-  public void setOndotRefId(String ondotRefId) {
-    this.ondotRefId = ondotRefId;
-  }
 }
